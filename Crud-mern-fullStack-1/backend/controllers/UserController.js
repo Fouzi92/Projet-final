0193405300a6b5d7ec87b94c86308a0e89819436
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-09-03 18:05:34 +0200

    Backend FINI 24MIN

--- Crud-mern-fullStack-1/backend/controllers/UserController.js
+++ Crud-mern-fullStack-1/backend/controllers/UserController.js
@@ -8,3 +8,43 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const saveUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const inserteduser = await user.save();
+    res.status(201).json(inserteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const updateduser = await User.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deleteuser = await User.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteuser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
