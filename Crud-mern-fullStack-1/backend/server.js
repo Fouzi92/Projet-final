9f1fdedd6d38092f9a3dd946b7eecb316ec671a2
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-09-03 16:45:00 +0200

    Backend data connect 13min

--- Crud-mern-fullStack-1/backend/server.js
+++ Crud-mern-fullStack-1/backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/crudmernstack", {
@@ -14,5 +15,6 @@ db.once("open", () => console.log("Database Connected"));
 
 app.use(cors());
 app.use(express.json());
+app.use(UserRoute);
 
 app.listen(5000, () => console.log("server up and running.."));
